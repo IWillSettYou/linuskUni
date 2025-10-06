7fa3981124e0384d50f361190203fc6f2202f1ab
IWillSettYoum <bmate20050911@gmail.com>
2025-10-06 15:34:49 +0200

    10.06

--- improg/gyak/10.06/igen.c
+++ improg/gyak/10.06/igen.c
@@ -41,15 +41,34 @@ int array_avg(const int arr[], const int len){
     return (double)sum / len;
     
 }
-
+ int str_cmp(char str1[], char str2[]){
+        int i = 0;
+        while('\0' != str1[i] && '\0' != str2[i]){
+            if(str1[i] != str2[i]){
+                
+            }
+            i++;
+        }
+        return str1[i] - str2[i];
+    }
+#include <string.h>
 int main(){
     /* int array_len = 252;
     scanf("%d", &array_len);
         BIG NO NO, HUGE NO NO!
     int vla[array_len]; */
 
-    const int array[] = {100,2,3,4,5,6,7,8,6,5,33,23,5,67,7,78,4,20000,2,4,6};
+    /* const int array[] = {100,2,3,4,5,6,7,8,6,5,33,23,5,67,7,78,4,20000,2,4,6};
     const int len = 21;
-    printf("%d",array_avg(array, len));
+    printf("%d",array_avg(array, len)); */
+   
 
+    const char *str1 = "kiscica";
+    const char *str2 = "kiscica";
+    const int cmp = str_cmp(str1, str2);
+    printf("cmp = %d\n", cmp);
+
+    const int official_cmp = strcmp(str1,str2);
+
+    const int strlen = strlen(str1);
 }
